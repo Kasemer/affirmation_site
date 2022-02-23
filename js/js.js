5fa9d34142f9e3e0ebadf232db6f5dc5c7b16d00
Kasemer <86676174+Kasemer@users.noreply.github.com>
2022-02-23 16:34:37 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,5 @@
+$( document ).ready(function(){
+	  $("button").click(function(){ // задаем функцию при нажатиии на элемент <button>
+	   $('.how').toggleClass('yes' ); // удаляем, или добавляем элементу <p> два класса
+	  });
+	});
